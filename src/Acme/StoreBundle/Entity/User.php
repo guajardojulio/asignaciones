b5ee52fde734ccdc3fd6a1ae0bc1789a7758937d
Julio Guajardo <guajardo.julio@gmail.com>
2016-08-31 23:55:58 -0300

    Ejercicio Tema 9

--- src/Acme/StoreBundle/Entity/User.php
+++ src/Acme/StoreBundle/Entity/User.php
@@ -239,5 +239,22 @@ class User implements AdvancedUserInterface, \Serializable
         return true;
     }
 
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
 
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
 }
